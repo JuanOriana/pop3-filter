6afda6f036d851d32ea3d79b5822ac86de79660f
tomas <tcerdeira@itba.edu.ar>
2021-10-22 14:14:50 -0300

    Fixed of some errors in proxy.c and refactor of enums / structs

--- include/proxy.h
+++ include/proxy.h
@@ -1,6 +1,4 @@
 #ifndef PROXY_POP3FILTER_H
 #define PROXY_POP3FILTER_H
 
-int build_passive(enum IP_TYPE ip_type);
-
 #endif
--- proxy.c
+++ proxy.c
@@ -12,9 +12,9 @@
 #include <limits.h>
 #include "./utils/include/logger.h"
 #include "./include/proxy.h"
-#include <selector.h>
-#include <buffer.h>
-#include <signal.h>
+#include "./utils/include/selector.h"
+#include "./utils/include/buffer.h"
+#include <sys/signal.h>
 
 #define max(n1, n2) ((n1) > (n2) ? (n1) : (n2))
 
@@ -26,55 +26,57 @@
 #define BUFFSIZE 1024
 #define MAX_PENDING_CONNECTIONS 3 // un valor bajo, para realizar pruebas
 
-enum IP_TYPE
+typedef enum
 {
     IPV4,
     IPV6
-};
+} IP_TYPE;
 
-typedef enum{
+typedef enum
+{
     RESOLVING,
     CONNECTING,
     COPYING,
     DONE,
-    ERROR,
-}proxy_state;
+} proxy_state;
 
-struct connection {
+typedef struct state_definition state_definition;
+struct state_definition
+{
+    proxy_state state;
+    const fd_handler handler;
+};
+
+struct connection
+{
     int fd_client;
     int fd_origin;
-    struct * state_definition;
+    state_definition *state_definition;
     buffer client_buffer;
     buffer origin_buffer;
-   
-    
 };
 
-struct state_definition{
-        proxy_state state;
-        const fd_handler *handler;
-};
-
-struct state_definition  client_states[] = {
-    { 
-    .state = RESOLVING,
-    .fd_handler.handle_read =NULL,
-    .fd_handler.handle_write =NULL,
-    .fd_handler.handle_close =NULL,
-    }, 
-    { 
-    .state = CONNECTING,
-    .fd_handler.handle_read =NULL,
-    .fd_handler.handle_write =NULL,
-    .fd_handler.handle_close =NULL,
+struct state_definition client_states[] = {
+    {
+        .state = RESOLVING,
+        .handler.handle_read = NULL,
+        .handler.handle_write = NULL,
+        .handler.handle_close = NULL,
     },
-    { 
-    .state = COPYING,
-    .fd_handler.handle_read =NULL,
-    .fd_handler.handle_write =NULL,
-    .fd_handler.handle_close =NULL,
-    }
-}
+    {
+        .state = CONNECTING,
+        .handler.handle_read = NULL,
+        .handler.handle_write = NULL,
+        .handler.handle_close = NULL,
+    },
+    {
+        .state = COPYING,
+        .handler.handle_read = NULL,
+        .handler.handle_write = NULL,
+        .handler.handle_close = NULL,
+    }};
+
+int build_passive(IP_TYPE ip_type);
 
 int main(int argc, char *argv[])
 {
@@ -85,7 +87,7 @@ int main(int argc, char *argv[])
     //     char *end     = 0;
     //     const long sl = strtol(argv[1], &end, 10);
 
-    //     if (end == argv[2]|| '\0' != *end 
+    //     if (end == argv[2]|| '\0' != *end
     //        || ((LONG_MIN == sl || LONG_MAX == sl) && ERANGE == errno)
     //        || sl < 0 || sl > USHRT_MAX) {
     //         logger(FATAL, "port should be an integer: %s\n", argv[1]);
@@ -97,90 +99,104 @@ int main(int argc, char *argv[])
 
     close(0); // Add an  extra FD to server
 
-    const char       *err_msg = NULL;
-    selector_status   ss      = SELECTOR_SUCCESS;
-    fd_selector selector      = NULL;
+    const char *err_msg = NULL;
+    selector_status ss = SELECTOR_SUCCESS;
+    fd_selector selector = NULL;
+    IP_TYPE ip_type = IPV4;
 
-    const int server = build_passive(IPV4);
-    if(server < 0) {
-        logger(FATAL,"Unable to establish connection");
+    const int server = build_passive(ip_type);
+    if (server < 0)
+    {
+        logger(FATAL, "Unable to establish connection");
     }
 
-    if(selector_fd_set_nio(server) == -1) {
+    if (selector_fd_set_nio(server) == -1)
+    {
         err_msg = "getting server socket flags";
         goto selector_finally;
     }
     const struct selector_init conf = {
         .signal = SIGALRM,
         .select_timeout = {
-            .tv_sec  = 10,
+            .tv_sec = 10,
             .tv_nsec = 0,
         },
     };
-    if(0 != selector_init(&conf)) {
+    if (0 != selector_init(&conf))
+    {
         err_msg = "initializing selector";
         goto selector_finally;
     }
 
     selector = selector_new(1024);
-    
-    if(selector == NULL) {
+
+    if (selector == NULL)
+    {
         err_msg = "unable to create selector";
         goto selector_finally;
     }
 
     const struct fd_handler passive_handler = {
-        .handle_read       = proxy_create_connection, // TODO: crear conexion activa y subscribirla al selector
-        .handle_write      = NULL,
-        .handle_close      = NULL, // nada que liberar
+        .handle_read = proxy_create_connection, // TODO: crear conexion activa y subscribirla al selector
+        .handle_write = NULL,
+        .handle_close = NULL, // nada que liberar
     };
-    
+
     ss = selector_register(selector, server, &passive_handler, OP_READ, NULL);
 
-    if(ss != SELECTOR_SUCCESS) {
+    if (ss != SELECTOR_SUCCESS)
+    {
         err_msg = "registering fd";
         goto selector_finally;
     }
-    
-    while(TRUE) {
+
+    while (TRUE)
+    {
         err_msg = NULL;
         ss = selector_select(selector);
-        if(ss != SELECTOR_SUCCESS) {
+        if (ss != SELECTOR_SUCCESS)
+        {
             err_msg = "serving";
             goto selector_finally;
         }
     }
-    if(err_msg == NULL) {
+    if (err_msg == NULL)
+    {
         err_msg = "closing";
     }
 
     int ret = 0;
-    
+
 selector_finally:
-    if(ss != SELECTOR_SUCCESS) {
-        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "": err_msg,
-                                  ss == SELECTOR_IO
-                                      ? strerror(errno)
-                                      : selector_error(ss));
+    if (ss != SELECTOR_SUCCESS)
+    {
+        fprintf(stderr, "%s: %s\n", (err_msg == NULL) ? "" : err_msg,
+                ss == SELECTOR_IO
+                    ? strerror(errno)
+                    : selector_error(ss));
         ret = 2;
-    } else if(err_msg) {
+    }
+    else if (err_msg)
+    {
         perror(err_msg);
         ret = 1;
     }
-    if(selector != NULL) {
+    if (selector != NULL)
+    {
         selector_destroy(selector);
     }
     selector_close();
 
     socksv5_pool_destroy();
 
-    if(server >= 0) {
+    if (server >= 0)
+    {
         close(server);
     }
     return ret;
 }
 
-int build_passive(enum IP_TYPE ip_type)
+int build_passive(IP_TYPE ip_type)
 {
     int opt = TRUE;
     int client_socket;
@@ -194,7 +210,7 @@ int build_passive(enum IP_TYPE ip_type)
         return -1;
     }
 
-    //set master socket to allow multiple connections , this is just a good habit, it will work without this
+    // set master socket to allow multiple connections , this is just a good habit, it will work without this
     if (setsockopt(client_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0)
     {
         log(ERROR, "set socket options failed");
@@ -240,9 +256,8 @@ int build_passive(enum IP_TYPE ip_type)
     return client_socket;
 }
 
-
-
-int proxy_create_connection(struct selector_key *key){
+int proxy_create_connection(struct selector_key *key)
+{
     struct sockaddr_storage client_address; // Client address
     // Set length of client address structure (in-out parameter)
     socklen_t client_address_len = sizeof(client_address);
@@ -254,14 +269,12 @@ int proxy_create_connection(struct selector_key *key){
         log(ERROR, "accept() failed");
         return -1;
     }
-    
+
     // client_socket is connected to a client!
     log(INFO, "Handling client %s", addrBuffer);
 
-    //TODO: CREAR HANDLER
-    ss = selector_register(key->s, client_socket,NULL , NULL, NULL); // Third parameter is null since non-handling is needed
-
-
+    // TODO: CREAR HANDLER
+    ss = selector_register(key->s, client_socket, NULL, NULL, NULL); // Third parameter is null since non-handling is needed
 
     return client_socket;
 }
