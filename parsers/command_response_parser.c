d825ae939ba6ebf6a409d7dcfa5838baeff4efd4
LucasCatolino <lcatolino@fi.uba.ar>
2021-11-16 12:20:34 -0300

    FInishing refactor on command_response_parser.c

--- parsers/command_response_parser.c
+++ parsers/command_response_parser.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -46,10 +45,8 @@ void command_response_parser_init(command_response_parser * parser) {
 
 command_response_state command_response_parser_feed(command_response_parser * parser, const char c, command_instance * command_to_respond) {
     command_instance * current_command = command_to_respond;
-    if(current_command == NULL){
+    if(current_command == NULL)
         parser->state = RESPONSE_ERROR;
-        log(ERROR, "command null");
-    }
 
     if(parser->state > RESPONSE_ERROR) {
         log(ERROR,"Response parser not reconize state: %d", parser->state);
@@ -57,134 +54,7 @@ command_response_state command_response_parser_feed(command_response_parser * pa
     else {
         command_response_handlers[parser->state](parser, c, current_command);
     }
-/*
-    switch(parser->state) {
-        case RESPONSE_INIT:
-            ///
-            if(c == positive_indicator_msg[0])
-                parser->state = RESPONSE_INDICATOR_POS;
-            else if(c == negative_indicator_msg[0])
-                parser->state = RESPONSE_INDICATOR_NEG;
-            else
-                parser->state = RESPONSE_ERROR;///
-            command_response_init(parser, c, current_command);
-            break;
 
-        case RESPONSE_INDICATOR_POS:
-            ///
-            if(c != positive_indicator_msg[parser->line_size])
-                parser->state = RESPONSE_ERROR;
-            else if(parser->line_size == positive_indicator_msg_size - 1) {
-                current_command->indicator = true;
-                parser->crlf_state = 0;
-                parser->state = RESPONSE_INDICATOR_MSG;
-            }///
-            command_response_indicator_pos(parser, c, current_command);
-            break;
-
-        case RESPONSE_INDICATOR_NEG:
-            ///
-            if(c != negative_indicator_msg[parser->line_size])
-                parser->state = RESPONSE_ERROR;
-            else if(parser->line_size == negative_indicator_msg_size - 1) {
-                current_command->indicator = false;
-                parser->crlf_state = 0;
-                parser->state = RESPONSE_INDICATOR_MSG;
-            }///
-            command_response_indicator_neg(parser, c, current_command);
-            break;
-
-        case RESPONSE_INDICATOR_MSG:
-            ///
-            if(c == crlf_inline_msg[0]) {
-                parser->crlf_state = 1;
-                parser->state = RESPONSE_INLINE_CRLF;
-            }///
-            command_response_indicator_msg(parser, c, current_command);
-            break;
-
-        case RESPONSE_INLINE_CRLF:
-            ///
-            if(c == crlf_inline_msg[parser->crlf_state++]) {
-                if(parser->crlf_state == crlf_inline_msg_size) {
-                    // -1 because its incremented at the end of the run
-                    parser->line_size     = -1;
-                    parser->crlf_state    =  2;
-                    // If is multiline AND we have a positive response, we continue, else we are absolutely done.
-                    if(current_command->indicator && current_command->is_multi) {
-                        parser->is_starting_body = true;
-                        parser->is_pipelining_possible = true;
-                        parser->state = RESPONSE_BODY;
-                    }
-                    else {
-                        parser->crlf_state    =  0;
-                        parser->state    = RESPONSE_INIT;
-                    }
-                }
-            } else
-                parser->state = RESPONSE_ERROR;///
-            command_response_inline_crlf(parser, c, current_command);
-            break;
-
-        case RESPONSE_BODY:
-            ///
-            parser->is_starting_body = false;
-            // CRLF HANDLING
-            if (c == crlf_multi_msg[0]){
-                parser->crlf_state = 1;
-            }
-            else if(c == crlf_multi_msg[1]) {
-                if(parser->crlf_state == 1) {
-                    parser->crlf_state = 2;
-                    parser->is_pipelining_possible = true;
-                    parser->line_size = -1;
-                } else
-                    parser->state = RESPONSE_ERROR;
-            }
-            else if(c == crlf_multi_msg[parser->crlf_state] && parser->crlf_state == 2) {
-                parser->state     = RESPONSE_MULTILINE_CRLF;
-                parser->crlf_state = 3;
-            }
-            // We are not parsing CRLF
-            else{
-                parser->crlf_state = 0;
-                if(!parser->includes_pipelining && parser->is_pipelining_possible && toupper(c) == pipelining_string[parser->line_size]){
-                    if (parser->line_size == pipelining_string_size-1){
-                        parser->includes_pipelining = true;
-                    }
-                }
-                else{
-                    parser->is_pipelining_possible = false;
-                }
-            }///
-            command_response_body(parser, c, current_command);
-            break;
-
-        case RESPONSE_MULTILINE_CRLF:
-            ///
-            if(c == crlf_multi_msg[parser->crlf_state++]) {
-                if(parser->crlf_state == crlf_multi_msg_size) {
-                    parser->state     = RESPONSE_INIT;
-                    parser->line_size  = -1;
-                    parser->crlf_state = 0;
-                }
-            } else if(parser->crlf_state == crlf_multi_msg_size - 1) {
-                parser->is_pipelining_possible = true;
-                parser->line_size  = -1;
-                parser->crlf_state = 0;
-                parser->state     = RESPONSE_BODY;
-            } else
-                parser->state = RESPONSE_ERROR;///
-            command_response_multiline_crlf(parser, c, current_command);
-            break;
-
-        case RESPONSE_ERROR:
-            break;
-
-        default:
-            log(ERROR,"Response parser not reconize state: %d", parser->state);
-    }
-*/
     if(parser->line_size++ == MAX_MSG_SIZE)
         parser->state = RESPONSE_ERROR;
     return parser->state;
@@ -225,7 +95,6 @@ command_response_state command_response_parser_consume_until(command_response_pa
 }
 
 static void command_response_init (command_response_parser * parser, const char c, __attribute__((unused)) command_instance * current_command) {
-    log(DEBUG, "EStoy en init: %d", parser->state);
     if(c == positive_indicator_msg[0])
         parser->state = RESPONSE_INDICATOR_POS;
     else if(c == negative_indicator_msg[0])
@@ -235,7 +104,6 @@ static void command_response_init (command_response_parser * parser, const char 
 }
 
 static void command_response_indicator_pos (command_response_parser * parser, const char c, command_instance * current_command) {
-    log(DEBUG, "EStoy en indicator pos: %d", parser->state);
     if(c != positive_indicator_msg[parser->line_size])
         parser->state = RESPONSE_ERROR;
     else if(parser->line_size == positive_indicator_msg_size - 1) {
@@ -246,7 +114,6 @@ static void command_response_indicator_pos (command_response_parser * parser, co
 }
 
 static void command_response_indicator_neg (command_response_parser * parser, const char c, command_instance * current_command) {
-    log(DEBUG, "EStoy en indicator neg: %d", parser->state);
     if(c != negative_indicator_msg[parser->line_size])
         parser->state = RESPONSE_ERROR;
     else if(parser->line_size == negative_indicator_msg_size - 1) {
@@ -257,7 +124,6 @@ static void command_response_indicator_neg (command_response_parser * parser, co
 }
 
 static void command_response_indicator_msg (command_response_parser * parser, const char c, __attribute__((unused)) command_instance * current_command) {
-    log(DEBUG, "EStoy en indicator msg: %d", parser->state);
     // Read message till \r, then a \n is expected (always)
     if(c == crlf_inline_msg[0]) {
         parser->crlf_state = 1;
@@ -266,7 +132,6 @@ static void command_response_indicator_msg (command_response_parser * parser, co
 }
 
 static void command_response_inline_crlf (command_response_parser * parser, const char c, command_instance * current_command) {
-    log(DEBUG, "EStoy en inline crlf: %d", parser->state);
     // I expect to complete a \r\n, then i evaluate if a multiline response follows or not
     if(c == crlf_inline_msg[parser->crlf_state++]) {
         if(parser->crlf_state == crlf_inline_msg_size) {
@@ -289,7 +154,6 @@ static void command_response_inline_crlf (command_response_parser * parser, cons
 }
 
 static void command_response_body (command_response_parser * parser, const char c, __attribute__((unused)) command_instance * current_command) {
-    log(DEBUG, "EStoy en response body: %d", parser->state);
     parser->is_starting_body = false;
     // CRLF HANDLING
     if (c == crlf_multi_msg[0]){
@@ -322,7 +186,6 @@ static void command_response_body (command_response_parser * parser, const char 
 }
 
 static void command_response_multiline_crlf (command_response_parser * parser, const char c, __attribute__((unused)) command_instance * current_command) {
-    log(DEBUG, "EStoy en multiline crlf: %d", parser->state);
     if(c == crlf_multi_msg[parser->crlf_state++]) {
         if(parser->crlf_state == crlf_multi_msg_size) {
             parser->state     = RESPONSE_INIT;
